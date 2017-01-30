91d0f42222c2b701514c0219e413afd5f1f8a688
andy <andymaheshw@gmail.com>
2017-01-30 07:41:04 -0500

    update for request library, just need url and to deploy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import request from 'request';
 
+
 const app = express();
 exports.app = app;
 
 app.get('/api/imagesearch/:search_term?', (req, res) => {
 	let search_term = req.params.search_term;
 	res.end(search_term);
+ 
+let options = {
+  url: 'https://api.github.com/repos/request/request',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+ 
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info.stargazers_count + " Stars");
+    console.log(info.forks_count + " Forks");
+  }
+}
+ 
+request(options, callback);
 
 })?
