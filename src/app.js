92e4c43ac32601c8ae9a810c69534496132c313c
andy <andymaheshw@gmail.com>
2017-02-05 11:52:12 -0500

    update for babelrc

--- src/app.js
+++ src/app.js
@@ -1,53 +1,66 @@
 import express from 'express';
 import request from 'request';
 import mongoose from 'mongoose';
+import Search from 'bing.search';
+import util from 'util';
 
 const app = express();
 exports.app = app;
 mongoose.Promise = global.Promise;
  
-export const app = express();
 mongoose.connect(process.env.MONGODB);
 
 // Set up the urlEntry schema
 var search_term = mongoose.Schema({
   original: String,
 });
-
-// I create an index here so it's faster to search by shortCode
-//urlEntrySchema.index({ shortCode: 1 });
  
 // Now, I create the model:
 var searchTerm = mongoose.model('searchTerm', search_term);
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("mongoose");
+});
+
 
 app.get('/api/imagesearch/:search_term?', (req, res) => {
 	let search_term = req.params.search_term;
-	res.end(search_term);
- 
-let options = {
-  url: 'https://api.github.com/repos/request/request',
-  headers: {
-    'User-Agent': 'request'
-  }
-};
- 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-  }
-  res.send(response);
-}
- 
-request(options, callback);
 
+	if (search_term)
+	{
+		var search_save = new searchTerm({ original: search_term });
+		search_save.save();
+		var options = {
+		 url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=cute animals',
+		  headers: {
+		    'Content-Type': 'multipart/form-data',
+		    'Ocp-Apim-Subscription-Key': process.env.SEARCH_KEY
+		  }
+		};
+		 
+		function callback(error, response, body) {
+		  if (!error && response.statusCode == 200) {
+		  	//console.log(body);
+		    var info = JSON.parse(body);
+		    res.send(info);
+		  }
+      }
+
+     request(options, callback);
+	}
+	      else {
+     	res.end("Nothing to search")
+ 		};
 });
 
-app.get('/api/latest/imagesearch/', (req, res) = {
+app.get('/api/latest/imagesearch/', (req, res) => {
 
 	searchTerm.find(function (err, search_terms) {
 		  if (err) return console.error(err);
 		  console.log(search_terms);
+		  res.send(search_terms)
 })
 
 });
